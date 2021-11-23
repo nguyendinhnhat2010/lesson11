8ba30a1352057b5d9ecdd37ea1e6c20f56c1342d
unknown <nguyendinhnhat2010@gmail.com>
2021-11-23 18:08:54 +0700

    done

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Shared preferences demo',
+      home: MyHomePage(title: 'Shared preferences demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    loadCounter();
+  }
+
+  // loading counter value on start
+  void loadCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      counter = (prefs.getInt('counter') ?? 0);
+    });
+  }
+
+  // tăng counter khi click chuột
+  void incrementCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', counter);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many time:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
